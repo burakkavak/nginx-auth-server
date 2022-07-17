c06f2110daa05a35683e376c28ac1ade4a361891
Burak Kavak <burak@kavak.dev>
2022-07-17 18:31:29 +0200

    implemented extensive form validation

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,42 +1,105 @@
-const forms = document.querySelectorAll("form");
+class LoginForm {
+  form: HTMLFormElement;
+  usernameInput: HTMLInputElement;
+  passwordInput: HTMLInputElement;
+  submitButton: HTMLButtonElement;
 
-forms.forEach((form) => {
-  const button = form.querySelector(".submit-button");
+  constructor(form: HTMLFormElement) {
+    this.form = form;
+    this.usernameInput = form.querySelector("#inputUsername");
+    this.passwordInput = form.querySelector("#inputPassword");
+    this.submitButton = form.querySelector('button[type="submit"]');
 
-  if (!button) {
-    console.error("fatal error: could not find submit button for form. define a button with the class 'submit-button' inside form");
+    if (!this.usernameInput || !this.passwordInput) {
+      throw new Error("error: username or password input is missing");
+    }
+
+    form.addEventListener("submit", (event) => this.onFormSubmit(event))
   }
 
-  form.addEventListener("submit", (event) => onFormSubmit(event))
-});
+  onFormSubmit(event: Event) {
+    event.preventDefault();
 
-function onFormSubmit(event: Event) {
-  event.preventDefault();
+    const form = <HTMLFormElement>event.currentTarget;
 
-  const form = <HTMLFormElement>event.currentTarget;
+    // validate form before sending POST request
+    if (form.classList.contains("needs-validation")) {
+      form.classList.add('was-validated')
 
-  // validate form before sending POST request
-  if (form.classList.contains("needs-validation")) {
-    form.classList.add('was-validated')
+      if (!form.checkValidity()) {
+        return;
+      }
+    }
 
-    if (!form.checkValidity()) {
+    if (!form.action) {
+      console.error("fatal error: no action defined for this form. cannot parse url for request");
       return;
     }
+
+    const formData = new FormData(form);
+
+    // replace button text with a spinner while the request is ongoing
+    const originalButtonHTML = this.submitButton.innerHTML;
+    const buttonWidth = this.submitButton.getBoundingClientRect().width;
+
+    this.submitButton.setAttribute("style", `width: ${buttonWidth}px;`);
+    this.submitButton.innerHTML = '<i class="fa-solid fa-circle-notch fa-spin"></i>';
+
+    this.toggleFormState();
+
+    fetch(form.action, {
+      method: "post",
+      body: formData
+    }).then((response) => {
+      if (response.ok) {
+        location.reload()
+      } else {
+        this.resetSubmitButton(originalButtonHTML);
+        this.toggleFormState();
+
+        this.usernameInput.setCustomValidity("Invalid credentials.");
+        this.passwordInput.setCustomValidity("Invalid credentials.");
+        this.submitButton.disabled = true;
+
+        // clear error messages after value changes on either input
+        this.usernameInput.addEventListener("input", () => {
+          this.usernameInput.setCustomValidity("");
+          this.passwordInput.setCustomValidity("");
+          this.submitButton.disabled = false;
+        }, { once: true });
+
+        this.passwordInput.addEventListener("input", () => {
+          this.usernameInput.setCustomValidity("");
+          this.passwordInput.setCustomValidity("");
+          this.submitButton.disabled = false;
+        }, { once: true });
+      }
+    }).catch((error) => {
+      this.resetSubmitButton(originalButtonHTML);
+      this.toggleFormState();
+      console.error(error);
+
+      // TODO: better error-handling when the server is offline for some reason
+    });
   }
 
-  if (!form.action) {
-    console.error("fatal error: no action defined for this form. cannot parse url for request");
-    return;
+  resetSubmitButton(originalHtml: string): void {
+    this.submitButton.innerHTML = originalHtml;
+    this.submitButton.removeAttribute("style");
   }
 
-  fetch(form.action, {
-    method: "post",
-    body: new FormData(form)
-  }).then((response) => {
-    if (response.ok) {
-      location.reload()
-    } else {
-      // TODO: form validation
-    }
-  });
+  /**
+   * Disables/enables form, inputs and buttons
+   */
+  toggleFormState(): void {
+    this.usernameInput.disabled = !this.usernameInput.disabled
+    this.passwordInput.disabled = !this.passwordInput.disabled
+    this.submitButton.disabled = !this.submitButton.disabled
+  }
+}
+
+const loginForm = <HTMLFormElement>document.querySelector("form.login-form");
+
+if (loginForm) {
+  new LoginForm(loginForm)
 }
