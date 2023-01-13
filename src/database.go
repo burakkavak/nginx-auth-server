d786838b461f321eb2e207309e017a05e3e63450
Burak Kavak <burak@kavak.dev>
2023-01-13 15:16:35 +0100

    updated database path but kept compatibility to old path

--- src/database.go
+++ src/database.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	bolt "go.etcd.io/bbolt"
+	"os"
 )
 
+var databaseFilePath string
+
+func init() {
+	databaseFilePath = fmt.Sprintf("%s/nginx-auth-server.db", GetExecutableDirectory())
+
+	if _, err := os.Stat(databaseFilePath); os.IsNotExist(err) {
+		// use new database file path in 'data'-subfolder if a database does not exist at the old path
+		databaseDirectoryPath := fmt.Sprintf("%s/data", GetExecutableDirectory())
+
+		if _, err := os.Stat(databaseDirectoryPath); os.IsNotExist(err) {
+			if err = os.MkdirAll(databaseDirectoryPath, 0770); err != nil {
+				appLog.Fatalf("fatal error: cannot create data directory at '%s'. %s", databaseDirectoryPath, err)
+			}
+		}
+
+		databaseFilePath = fmt.Sprintf("%s/nginx-auth-server.db", databaseDirectoryPath)
+	}
+}
+
 func initDatabase() *bolt.DB {
-	db, err := bolt.Open(fmt.Sprintf("%s/nginx-auth-server.db", GetExecutableDirectory()), 0660, nil)
+	// TODO: can cause delays on concurrent calls
+	db, err := bolt.Open(databaseFilePath, 0660, nil)
 
 	if err != nil {
 		appLog.Fatalf("could not open/create database. %s\n", err)
